8403d0c1d95f80da774d8c277b48ef7ce1ef7d3d
jalal.mammadli <jmammadli2021@ada.edu.az>
2021-11-26 15:11:40 +0200

    topnav-update

---
+++ src/components/navbar/buttons/navCart.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './navCart.scss';
+
+const NavCart = () => {
+    
+    return (
+        <div className='nav__cart'>
+            <ul className='nav__cart__ul'>
+                <li><a href="">$44.50</a></li>
+                <li><a href="">View Cart</a></li>
+                <li><a href="">Checkout</a></li>
+            </ul>
+        </div>
+    );
+};
+
+export default NavCart;
+
