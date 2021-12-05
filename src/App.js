59122c5d6fb02fc6bd9ae913f7aecff6ea9c9ac9
jalal.mammadli <jmammadli2021@ada.edu.az>
2021-12-05 15:15:25 +0200

    about

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Navbar from './components/Navbar';
 import Carousel from './components/Carousel';
+import About from './components/About';
 
 function App() {
   return (
     <main>
       <Navbar />
       <Carousel />
+      <About />
     </main>
     
   );
