0d10885ef9bc96d68ba6a8ac0d3c2449b73d422e
jalal.mammadli <jmammadli2021@ada.edu.az>
2021-12-06 16:19:25 +0200

    about-update

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,18 +7,18 @@ import aboutImage2 from '../../assets/img/about_img_2.png';
 
 const About = () => {
     const [about] = useState(Structure['about']);
-    const {title, subTitle, text} = about;
 
     return (
         <div className="about">
             <div className="about__images">
-                <img src={aboutImage2} alt="" className="about__images__one"/>
-                <img src={aboutImage1} alt="" className="about__images__two"/>
+                <img src={aboutImage2} alt="" className="about__images__one" />
+                <img src={aboutImage1} alt="" className="about__images__two" />
             </div>
+            
             <div className="about__texts">
-                <h1 className="about__texts__title">{title}</h1>
-                <h3 className="about__texts__subTitle">{subTitle}</h3>
-                <p className="about__texts__paragraph">{text}</p>
+                <h1 className="about__texts__title">{about.title}</h1>
+                <h3 className="about__texts__subTitle">{about.subTitle}</h3>
+                <p className="about__texts__paragraph">{about.text}</p>
             </div>
         </div>
     );
