e43d0c4365dca7222e72460b95b7af85672f5e8e
jalal.mammadli <jmammadli2021@ada.edu.az>
2021-12-07 12:08:06 +0200

    gallery-update

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -4,17 +4,17 @@ import Structure from '../../structure.json';
 
 const Gallery = () => {
     const [gallery] = useState(Structure['gallery']);
-    const {title, subTitle, galleryImages} = gallery;
 
     return(
         <div className="gallery">
             <div className="gallery__texts">
-                <h1>{title}</h1>
-                <h3>{subTitle}</h3>
+                <h1>{gallery.title}</h1>
+                <h3>{gallery.subTitle}</h3>
             </div>
+            
             <div className="gallery__images">
-                {galleryImages.map((gallery) =>{
-                    return ( <img src={gallery['path']} />);
+                {gallery.galleryImages.map((gallery, index) => {
+                    return ( <img src={gallery['path']} key ={index} /> );
                 })}
             </div>
         </div>
